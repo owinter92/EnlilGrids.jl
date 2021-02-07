4ac29c549abf464779293e21de365d311c9dbdcf
Ondrej Winter <ondrej.winter@fs.cvut.cz>
2021-02-07 19:20:40 +0100

    Update tests: load_gmsh_file.

--- test/gmsh_test.jl
+++ test/gmsh_test.jl
@@ -1,3 +1,4 @@
 @testset "load_gmsh_file" begin
     @test_throws ErrorException("File a does not exist.") load_gmsh_file("a")
+    @test_throws ErrorException("Gmsh mesh file format saved in binary mode. Please convert to ASCII mode.") load_gmsh_file("data/gmsh_v41_1_bin.msh")
 end
