402351224f7b9c3a0c8bb389e80f528aa21d3ef4
Ondrej Winter <ondrej.winter@fs.cvut.cz>
2021-02-22 23:15:33 +0100

    code cov

--- test/gmsh_test.jl
+++ test/gmsh_test.jl
@@ -52,18 +52,20 @@ end
     @test gmsh_do_elements(msh.raw_Elements) == test_elements
 end
 
-#@testset "load_gmsh" begin
-#    msh = (physicalTagtoName = (physicalTag1DtoName = Dict(4 => "Line4",2 => "Line2",3 => "Line3",1 => "Line1"),
-#           physicalTag2DtoName = Dict(5 => "Surface1"), physicalTag3DtoName = Dict{Int64,String}()),
-#           tagToPhysicalTag = (pointTagtoPhysicalTag = Dict{Int64,Int64}(),
-#           curveTagtoPhysicalTag = Dict(4 => 4,2 => 2,3 => 3,1 => 1),
-#           surfaceTagtoPhysicalTag = Dict(1 => 5),
-#           volumeTagtoPhysicalTag = Dict{Int64,Int64}()),
-#           nodes = (nodeTag = [1, 2, 3, 4, 5], vx = [0.0, 1.0, 1.0, 0.0, 0.5], vy = [0.0, 0.0, 1.0, 1.0, 0.5], vz = [0.0, 0.0, 0.0, 0.0, 0.0]),
-#           elements = (entityTag1D = [1, 2, 3, 4], entityTag2D = [1, 1, 1, 1], entityTag3D = Int64[], elementType1D = [1, 1, 1, 1], elementType2D = [2, 2, 2, 2], elementType3D = Int64[], elementTag1D = [1, 2, 3, 4], elementTag2D = [5, 6, 7, 8], elementTag3D = Int64[], nodeTags1D = [[1, 2], [2, 3], [3, 4], [4, 1]], nodeTags2D = [[1, 2, 5], [4, 1, 5], [2, 3, 5], [3, 4, 5]], nodeTags3D = Array{Int64,1}[]))
-#    @test load_gmsh("data/gmsh_v41_2d_verycoarse.msh") == msh
-#end
-#
+@testset "load_gmsh" begin
+    include("data/gmsh/gmsh41_triangles_coarse.jl")
+    @test load_gmsh("data/gmsh/gmsh41_triangles_coarse.msh") == (physicalTagtoName=test_physicals,
+         tagToPhysicalTag=test_entities,
+         nodes=test_nodes,
+         elements=test_elements)
+
+    include("data/gmsh/gmsh41_tetrahedrons_coarse.jl")
+    @test load_gmsh("data/gmsh/gmsh41_tetrahedrons_coarse.msh") == (physicalTagtoName=test_physicals,
+         tagToPhysicalTag=test_entities,
+         nodes=test_nodes,
+         elements=test_elements)
+end
+
 #@testset "import_gmsh" begin
 #    using MeshCore.Exports
 #    using StaticArrays
