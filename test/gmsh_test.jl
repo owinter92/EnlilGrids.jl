b6a656edbaeaabf611141604f1265c19279f8f69
Ondrej Winter <ondrej.winter@fs.cvut.cz>
2021-02-22 23:10:11 +0100

    code cov

--- test/gmsh_test.jl
+++ test/gmsh_test.jl
@@ -40,33 +40,18 @@ end
     include("data/gmsh/gmsh41_tetrahedrons_coarse.jl")
     msh = load_gmsh_file("data/gmsh/gmsh41_tetrahedrons_coarse.msh")
     @test gmsh_do_nodes(msh.raw_Nodes) == test_nodes
-
-
-#    msh=load_gmsh_file("data/gmsh_v41_3d_verycoarse.msh")
-#    nodes=(nodeTag = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
-#           vx = [0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.5, 0.5, 0.5, 1.0, 0.5, 0.0],
-#           vy = [0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.5, 0.5, 0.0, 0.5, 1.0, 0.5],
-#           vz = [0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0, 0.5, 0.5, 0.5, 0.5])
-#    @test gmsh_do_nodes(msh[:raw_Nodes]) == nodes
 end
-#
-#@testset "gmsh_do_elements" begin
-#    msh=load_gmsh_file("data/gmsh_v41_3d_verycoarse.msh")
-#    elements=(entityTag1D = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-#              entityTag2D = [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6],
-#              entityTag3D = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-#              elementType1D = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-#              elementType2D = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
-#              elementType3D = [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
-#              elementTag1D = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
-#              elementTag2D = [21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44],
-#              elementTag3D = [45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68],
-#              nodeTags1D = [[1, 2], [2, 3], [3, 4], [4, 1], [5, 6], [6, 7], [7, 8], [8, 5], [1, 5], [2, 6], [3, 7], [4, 8]],
-#              nodeTags2D = [[1, 2, 9], [4, 1, 9], [2, 3, 9], [3, 4, 9], [5, 6, 10], [8, 5, 10], [6, 7, 10], [7, 8, 10], [1, 2, 11], [5, 1, 11], [2, 6, 11], [6, 5, 11], [2, 3, 12], [6, 2, 12], [3, 7, 12], [7, 6, 12], [3, 4, 13], [7, 3, 13], [4, 8, 13], [8, 7, 13], [4, 1, 14], [1, 5, 14], [8, 4, 14], [5, 8, 14]],
-#              nodeTags3D = [[12, 9, 14, 13], [11, 9, 14, 12], [14, 10, 11, 12], [14, 10, 12, 13], [14, 5, 1, 11], [11, 10, 5, 6], [8, 5, 14, 10], [9, 4, 1, 14], [14, 4, 8, 13], [1, 9, 11, 2], [3, 4, 9, 13], [6, 12, 10, 7], [10, 8, 7, 13], [12, 7, 3, 13], [11, 12, 6, 2], [3, 12, 9, 2], [1, 9, 14, 11], [5, 14, 10, 11], [10, 14, 8, 13], [7, 12, 10, 13], [9, 4, 14, 13], [3, 9, 12, 13], [10, 11, 12, 6], [12, 11, 9, 2]])
-#    @test gmsh_do_elements(msh[:raw_Elements]) == elements
-#end
-#
+
+@testset "gmsh_do_elements" begin
+    include("data/gmsh/gmsh41_triangles_coarse.jl")
+    msh = load_gmsh_file("data/gmsh/gmsh41_triangles_coarse.msh")
+    @test gmsh_do_elements(msh.raw_Elements) == test_elements
+
+    include("data/gmsh/gmsh41_tetrahedrons_coarse.jl")
+    msh = load_gmsh_file("data/gmsh/gmsh41_tetrahedrons_coarse.msh")
+    @test gmsh_do_elements(msh.raw_Elements) == test_elements
+end
+
 #@testset "load_gmsh" begin
 #    msh = (physicalTagtoName = (physicalTag1DtoName = Dict(4 => "Line4",2 => "Line2",3 => "Line3",1 => "Line1"),
 #           physicalTag2DtoName = Dict(5 => "Surface1"), physicalTag3DtoName = Dict{Int64,String}()),
